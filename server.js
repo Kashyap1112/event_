3074658b633cfb5ce8e668e093a434cac691db1d
Thakkar <kashthakkar1112@gmail.com>
2025-02-28 15:36:02 +0530

    first commit

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-require('dotenv').config();
-const connectDB = require('./config/db');
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+connectDB();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-connectDB();
+app.use("/api/users", require("./routes/userRoutes"));
+app.use("/api/events", require("./routes/eventRoutes"));
+app.use("/api/register", require("./routes/registrationRoutes"));
 
-app.use('/api/users', require('./routes/userRoutes'));
-app.use('/api/events', require('./routes/eventRoutes'));
-app.use('/api/register', require('./routes/registrationRoutes'));
-
-app.listen(5000, () => console.log("Server running on port 5000"));
+app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
